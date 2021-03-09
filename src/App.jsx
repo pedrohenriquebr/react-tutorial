dae820088d81c806ce55ba327a0afe0925e336b1
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-03-09 09:13:40 -0300

    Add removeCharacter prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
 import React, {Component} from 'react';
 import Table from './Table';
 
-class App extends Component { 
+class App extends Component {
+    state = {
+      characters: [
+        {
+          name: 'Charlie',
+          job: 'Janitor',
+        },
+        {
+          name: 'Mac',
+          job: 'Bouncer',
+        },
+        {
+          name: 'Dee',
+          job: 'Aspring actress',
+        },
+        {
+          name: 'Dennis',
+          job: 'Bartender',
+        },
+      ]
+    }
+
+    removeCharacter = (index) => {
+      const {characters} = this.state
+    
+      this.setState({
+        characters: characters.filter((character, i) => {
+          return i !== index
+        }),
+      })
+    }
+    
     render(){
-
-        const characters = [
-            {
-              name: 'Charlie',
-              job: 'Janitor',
-            },
-            {
-              name: 'Mac',
-              job: 'Bouncer',
-            },
-            {
-              name: 'Dee',
-              job: 'Aspring actress',
-            },
-            {
-              name: 'Dennis',
-              job: 'Bartender',
-            },
-          ];
-
-
       return (
         <div className="container" >
-            <Table characterData={characters}/>
+            <Table characterData={this.state.characters} removeCharacter={this.removeCharacter}/>
         </div>
       )
     }
---
+++ src/Form.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+    initState = {
+        name: '',
+        job: ''
+    }
+}
